388a378f3fef019bacc37598a29e986729bde1fc
allanling <allan.ling@technologyadvice.com>
2024-06-22 20:22:27 +0800

    test mf

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ObjectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EnsureTokenIsValid;
+
 
 Route::controller(ObjectController::class)
     ->prefix('object')->group(function () {
@@ -9,3 +12,13 @@ Route::controller(ObjectController::class)
         Route::get('/{key}', 'show');
         Route::post('/', 'store');
     });
+
+
+Route::middleware([EnsureTokenIsValid::class])->group(function () {
+    Route::controller(UserController::class)->prefix('auth')->group(function () {
+        Route::post('/close', 'close');
+        Route::patch('/users/{user_id}', 'update');
+        Route::get('/users/{user_id}', 'show');
+        Route::post('/signup', 'signup')->withoutMiddleware([EnsureTokenIsValid::class]);
+    });
+});
